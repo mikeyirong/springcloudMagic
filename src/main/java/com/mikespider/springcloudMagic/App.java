2048372449cc010a0223bbb32ec39f2a7101a020
mike <1135556405@qq.com>
2017-10-24 18:27:49 +0800

    ccc

--- src/main/java/com/mikespider/springcloudMagic/App.java
+++ src/main/java/com/mikespider/springcloudMagic/App.java
@@ -1,12 +1,24 @@
 package com.mikespider.springcloudMagic;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class App {
+	 @Value("${config.name}")
+	  String name = "World";
+
+	  @RequestMapping("/")
+	  public String home() {
+	    return "Hello " + name;
+	  }
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
